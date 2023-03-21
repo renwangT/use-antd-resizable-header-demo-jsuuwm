f6a4f780bf16bdd804d15c249cabbbf954bd7a39
唐仁旺 <357250839@qq.com>
2023-03-21 23:08:18 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Table } from 'antd';
 import { useAntdResizableHeader } from '@minko-fe/use-antd-resizable-header';
 import '@minko-fe/use-antd-resizable-header/dist/style.css';
+import ReactDragListView from 'react-drag-listview';
 
 import './App.css';
 
-const columns: any[] = [
+const initColumns: any[] = [
   {
     title: 'Name',
     dataIndex: 'name',
@@ -15,14 +16,14 @@ const columns: any[] = [
     filters: [
       {
         text: 'Joe',
-        value: 'Joe',
+        value: 'Joe'
       },
       {
         text: 'John',
-        value: 'John',
-      },
+        value: 'John'
+      }
     ],
-    onFilter: (value, record) => record.name.indexOf(value) === 0,
+    onFilter: (value, record) => record.name.indexOf(value) === 0
   },
   {
     title: 'Other',
@@ -32,7 +33,7 @@ const columns: any[] = [
         dataIndex: 'age',
         key: 'age',
         width: 150,
-        sorter: (a, b) => a.age - b.age,
+        sorter: (a, b) => a.age - b.age
       },
       {
         title: 'Address',
@@ -41,7 +42,7 @@ const columns: any[] = [
             title: 'Street',
             dataIndex: 'street',
             key: 'street',
-            width: 150,
+            width: 150
           },
           {
             title: 'Block',
@@ -50,19 +51,19 @@ const columns: any[] = [
                 title: 'Building',
                 dataIndex: 'building',
                 key: 'building',
-                width: 100,
+                width: 100
               },
               {
                 title: 'Door No.',
                 dataIndex: 'number',
                 key: 'number',
-                width: 100,
-              },
-            ],
-          },
-        ],
-      },
-    ],
+                width: 100
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
   {
     title: 'Company',
@@ -71,22 +72,22 @@ const columns: any[] = [
         title: 'Company Address',
         dataIndex: 'companyAddress',
         key: 'companyAddress',
-        width: 200,
+        width: 200
       },
       {
         title: 'Company Name',
         dataIndex: 'companyName',
-        key: 'companyName',
-      },
-    ],
+        key: 'companyName'
+      }
+    ]
   },
   {
     title: 'Gender',
     dataIndex: 'gender',
     key: 'gender',
     width: 80,
-    fixed: 'right',
-  },
+    fixed: 'right'
+  }
 ];
 
 const data: any[] = [];
@@ -100,24 +101,35 @@ for (let i = 0; i < 100; i++) {
     number: 2035,
     companyAddress: 'Lake Street 42',
     companyName: 'SoftLake Co',
-    gender: 'M',
+    gender: 'M'
   });
 }
 
 export default function App() {
+  const [columns, setColumns] = React.useState<any[]>(initColumns);
   const { components, resizableColumns, tableWidth } = useAntdResizableHeader({
-    columns,
+    columns
   });
-
+  const dragProps = {
+    onDragEnd(fromIndex: number, toIndex: number) {
+      const newCols = [...columns];
+      const item = newCols.splice(fromIndex, 1)[0];
+      newCols.splice(toIndex, 0, item);
+      setColumns(newCols);
+    },
+    nodeSelector: 'th'
+  };
   return (
     <div>
       <h1>Hello!</h1>
-      <Table
-        columns={resizableColumns}
-        components={components}
-        dataSource={data}
-        scroll={{ x: tableWidth }}
-      />
+      <ReactDragListView.DragColumn {...dragProps}>
+        <Table
+          columns={resizableColumns}
+          components={components}
+          dataSource={data}
+          scroll={{ x: tableWidth }}
+        />
+      </ReactDragListView.DragColumn>
     </div>
   );
 }
